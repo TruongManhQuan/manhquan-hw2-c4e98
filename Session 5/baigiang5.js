fc424137226b2cd31c603bdd298705b134b770f4
manhquan <tjmberlake@gmail.com>
2020-10-13 21:02:46 +0700

    add check point

---
+++ Session 5/baigiang5.js
@@ -0,0 +1,9 @@
+// // cau 1: tim 1 so ngau tu 0 - 10
+// let a = Math.floor(Math.random() * 10);
+// console.log(a);
+
+// cau 2: 
+// let myArray = [2,5,6,9,10];
+
+// let randomValue = myArray[Math.floor(Math.random() * myArray.length)];
+// console.log(randomValue);
